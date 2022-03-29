b2a03f442cd1c829afbc1abddd6e72ee44fcc65f
Rose Zhan <rose@hashicorp.com>
2022-03-29 21:55:33 +0000

    added an s3 module

---
+++ s3-bucket.tf
@@ -0,0 +1,11 @@
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.9.0"
+  # insert the 5 required variables here
+  bucket_prefix = var.prefix
+
+  tags = {
+      Environment = "test"
+      Name = "rosezhan"
+  }
+}
