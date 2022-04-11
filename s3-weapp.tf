c33a6663e5b8d96860dbdfbff490c9443bfc44ba
FangliZhan <31291225+FangliZhan@users.noreply.github.com>
2022-04-11 10:51:16 -0500

    Update and rename s3-bucket.tf to s3-weapp.tf

---
+++ s3-weapp.tf
@@ -0,0 +1,11 @@
+module "s3-webapp" {
+  source  = "app.terraform.io/dcatcher-learning/s3-webapp/aws"
+  name   = var.name
+  region = var.region
+  prefix = var.prefix
+  version = "1.0.1"
+    tags = {
+      Environment = "test"
+      Name = "rosezhan"
+  }    
+}
