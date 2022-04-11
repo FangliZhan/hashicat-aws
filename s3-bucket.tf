9b685746b5671c0b1cf42b3333c5ee1afea7300c
FangliZhan <31291225+FangliZhan@users.noreply.github.com>
2022-04-11 10:41:56 -0500

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,10 @@
-module "s3-bucket" {
+/* module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   # version = "2.9.0"
+  version = "2.15.0" */
+  
+module "s3-bucket" {
+  source  = "app.terraform.io/dcatcher-learning/s3-bucket/aws"
   version = "2.15.0"
   
   # insert the 5 required variables here
